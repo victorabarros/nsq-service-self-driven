77a28fe1cf19a7a58d30a8cf1bad66dee3fa7fde
Victor Barros <victorbarros1130@gmail.com>
2020-04-05 17:09:54 -0300

    refact

--- run.sh
+++ run.sh
@@ -1,35 +1,33 @@
-echo "                                                       " && sleep .25
-echo "                                                       " && sleep .25
-echo "                   _ __   ___   __ _                   " && sleep .25
-echo "                  | '_ \ / __| / _' |                  " && sleep .25
-echo "                  | | | |\__ \| (_| |                  " && sleep .25
-echo "                  |_| |_||___/ \__, |                  " && sleep .25
-echo "                                  | |                  " && sleep .25
-echo "                                  |_|                  " && sleep .25
-echo "            _   __  ______        _                    " && sleep .25
-echo "           | | / _| |  _  \      (_)                   " && sleep .25
-echo " ___   ___ | || |_  | | | | _ __  _ __   __ ___  _ __  " && sleep .25
-echo "/ __| / _ \| ||  _| | | | || '__|| |\ \ / // _ \| '_ \ " && sleep .25
-echo "\__ \|  __/| || | _ | |/ / | |   | | \ V /|  __/| | | |" && sleep .25
-echo "|___/ \___||_||_|(_)|___/  |_|   |_|  \_/  \___||_| |_|" && sleep .25
-echo "                                                       " && sleep .25
-echo "                                                       " && sleep .25
+echo "                   _ __   ___   __ _                   " && sleep .2
+echo "                  | '_ \ / __| / _' |                  " && sleep .2
+echo "                  | | | |\__ \| (_| |                  " && sleep .2
+echo "                  |_| |_||___/ \__, |                  " && sleep .2
+echo "                                  | |                  " && sleep .2
+echo "                                  |_|                  " && sleep .2
+echo "            _   __  ______        _                    " && sleep .2
+echo "           | | / _| |  _  \      (_)                   " && sleep .2
+echo " ___   ___ | || |_  | | | | _ __  _ __   __ ___  _ __  " && sleep .2
+echo "/ __| / _ \| ||  _| | | | || '__|| |\ \ / // _ \| '_ \ " && sleep .2
+echo "\__ \|  __/| || | _ | |/ / | |   | | \ V /|  __/| | | |" && sleep .2
+echo "|___/ \___||_||_|(_)|___/  |_|   |_|  \_/  \___||_| |_|" && sleep .2
+echo "                                                       " && sleep .2
+echo "                                                       " && sleep .2
 # from http://patorjk.com/software/taag font doom
 
 
 echo && echo "Kill containers and images: "
-docker rm -f nsq-service-self-driven_nsqadmin_1
-docker rm -f nsq-service-self-driven_nsqd_1
-docker rm -f nsq-service-self-driven_nsqlookupd_1
-docker rm -f nsq-service-self-driven_writer_1
-docker rm -f nsq-service-self-driven_pyreader_1
-docker rm -f nsq-service-self-driven_goreader_1
+docker rm -f nsq-service-self-driven_nsqadmin_1 \
+    nsq-service-self-driven_nsqd_1 \
+    nsq-service-self-driven_nsqlookupd_1 \
+    nsq-service-self-driven_writer_1 \
+    nsq-service-self-driven_pyreader_1 \
+    nsq-service-self-driven_goreader_1
 # Images:
-docker rmi -f nsqio/nsq:v1.2.0
-docker rmi -f nsq-service-self-driven_writer
-docker rmi -f nsq-service-self-driven_pyreader
-docker rmi -f nsq-service-self-driven_goreader
-docker rmi -f golang:1.14-alpine
+docker rmi -f nsqio/nsq:v1.2.0 \
+    nsq-service-self-driven_writer \
+    nsq-service-self-driven_pyreader \
+    nsq-service-self-driven_goreader \
+    golang:1.14-alpine
 
 echo && echo "compose up: "
 docker-compose up -d  # -d to don't lock terminal with logs
